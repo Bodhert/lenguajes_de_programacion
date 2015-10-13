a485407b71a1dbf7fddc95405ae213d60750fdd5
Alejandro Cordoba Bodhert <alejandro.cordoba.bodhert@gmail.com>
2015-10-13 18:26:26 +0000

    clase 25

---
+++ seguimiento/clase25/entrada02.cpp
@@ -0,0 +1,32 @@
+
+/*
+ * fichero: entrada02.cpp
+ * compilar: $ g++ -o entrada02 entrada02.cpp
+ *
+ * ejecutar: $ ./entrada02
+ *
+ * proposito: Mostrar como se leen datos de la entrada estandar
+ *            sin formato.
+ */
+#include <iostream>
+
+using namespace std;
+
+
+int
+main() {
+  const streamsize TOTALDATOS = 10;
+  char* elementos = new char[TOTALDATOS];
+
+  cout << "Entre un conjunto de caracteres: ";
+  cin.read(elementos, TOTALDATOS);
+
+  if (!cin) {
+    cerr << "Problemas en el flujo de entrada" << endl;
+  }
+  else {
+    cout << elementos << endl;
+  }
+
+  return 0;
+}
