a4e7fe671df968e32ee96f5a5aa69f97b8ae79db
Alejandro Cordoba Bodhert <alejandro.cordoba.bodhert@gmail.com>
2015-11-05 18:25:48 +0000

    trabajando con ruby y sus colecciones

---
+++ seguimiento/clase30/casewhen.rb
@@ -0,0 +1,32 @@
+def verificar a
+  case a
+  when 1
+    puts "es uno"
+  when 2
+    puts "es dos"
+  else
+    puts "es otro valor"
+  end
+end
+
+def verificar2 a
+  case a
+  when "Uno"
+    puts 1
+  when "Dos"
+    puts 2
+  else
+    puts 0
+  end
+end
+
+def verificar3 a
+  case a
+  when "Uno"
+    1
+  when "Dos"
+    2
+  else
+    0
+  end
+end
